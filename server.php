16c658d7abf6c6dd0e5f032f279f81389538949d
ralf <rb@egroupware.org>
2026-09-07 19:25:39 +0200

    fix PHP Warning: unknown array key "sessionid"

--- server.php
+++ server.php
@@ -64,9 +64,8 @@ $valid_authorization = [
 $server->on('open', function (Swoole\Websocket\Server $server, Swoole\Http\Request $request)
 {
 	//var_dump($request);
-	$sessionid = $request->cookie['sessionid'];	// Api\Session::EGW_SESSION_NAME
-	$session = new EGroupware\SwoolePush\Session($sessionid); //, 'memcached1:11211,memcached2:11211', 'memcached');
-	if (!$session->exists())
+	if (!($sessionid = $request->cookie['sessionid'] ?? null) ||
+	    !(new EGroupware\SwoolePush\Session($sessionid))->exists())
 	{
 		error_log("server: handshake success with fd{$request->fd}, FAILED with unknown sessionid=$sessionid");
 		$server->close($request->fd);
